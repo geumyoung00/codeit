072721e100fa28335661ab82ca912c15f11927bb
젤리 <stay@jelliui-MacBookAir.local>
2025-05-05 07:27:37 +0900

    Setup routes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,21 +1,64 @@
 'use client';
 
 import { Button } from '@/components/Buttons/Button';
-import { InputCheck } from '@/components/Input/InputCheck';
 import { InputText } from '@/components/Input/InputText';
 import { Wrapper } from '@/components/Wrapper';
+import { todoProps } from '@/types/todos';
+import { useState } from 'react';
+import { TodoSection } from '../components/ToDo';
+import ImgTodo from '@/assets/img_todo.svg';
+import ImgDone from '@/assets/img_done.svg';
+import EmptyTodo from '@/assets/img_todo_empty.svg';
+import EmptyDone from '@/assets/img_done_empty.svg';
+import { handleInputChange } from '@/actions/inputTextAction';
+import { addToDo, message } from '@/actions/todoAction';
 
 export default function Home() {
+  const [todos, setTodos] = useState<todoProps[]>([]);
+  const [text, setText] = useState<string | undefined>();
+  const handleChange = handleInputChange(setText);
+
+  const onSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    addToDo(text);
+    if (message) {
+      console.log(message);
+    }
+  };
+
   return (
     <Wrapper>
-      <InputText />
-      <InputCheck id='test' label={'테스트'} />
-      <InputCheck isDetailed id='detailTest' label={'테스트'} />
-      <Button />
-      <Button type='empty' />
-      <Button type='delete' />
-      <Button type='save' />
-      <Button type='edit' />
+      <form onSubmit={onSubmit} className='w-full flex gap-8 tab:gap-16'>
+        <InputText text={text} onChange={handleChange} />
+        {todos ? <Button /> : <Button type='empty' />}
+      </form>
+
+      <section className='mt-40 flex flex-col gap-48 desk:grid desk:grid-cols-2 desk:gap-24'>
+        <TodoSection
+          title='To Do'
+          img={ImgTodo}
+          emptyImg={EmptyTodo}
+          message={
+            <>
+              할 일이 없어요.
+              <br />
+              TODO를 새롭게 추가해주세요!
+            </>
+          }
+        />
+        <TodoSection
+          title='Done'
+          img={ImgDone}
+          emptyImg={EmptyDone}
+          message={
+            <>
+              아직 다 한 일이 없어요.
+              <br />
+              해야 할 일을 체크해보세요!
+            </>
+          }
+        />
+      </section>
     </Wrapper>
   );
 }
---
+++ src/app/todos/[id]/page.tsx
@@ -0,0 +1,11 @@
+'use client';
+
+import { Wrapper } from '@/components/Wrapper';
+
+export default function DetailPage() {
+  return (
+    <Wrapper>
+      <h5>할일상세보기</h5>
+    </Wrapper>
+  );
+}
